14484bc3c1831f2249bb76944a3f04aaa147795b
Muhammad Bilal <ahmad_4al@hotmail.com>
2023-03-30 01:54:02 +0500

    App.js Update

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ const emailRouter = require('./routes/emailRoute');
 const passRouter = require('./routes/forgotpassRoute');
 const app = express();
 const otpRouter = require('./routes/otpRoute')
-
+const env = require("dotenv").config(); 
+const process = require('process');
+const { PORT } = process.env;
 const Studentrouter = require('./routes/studentRoute')
 const Alertrouter = require('./routes/AlertRoute')
 
@@ -19,11 +21,11 @@ app.use('',Alertrouter)
 
 
 
-
+mongoose.set('strictQuery', true)
 mongoose.connect('mongodb://localhost:27017/SchoolAppBackend')
 .then(()=>{
-    app.listen(4000,()=>{
-        console.log('Connect and app listen on port 4000')
+    app.listen(PORT,()=>{
+        console.log(`Connect App listen on port ${PORT}`)
     })
 }).catch((err)=>{
 console.log(err);
